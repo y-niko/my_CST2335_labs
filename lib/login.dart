cd687d41806f2ec72690ac8ffe91109027f139c9
Siqin Yang <yang0425@algonquinlive.com>
2025-05-27 11:36:57 -0400

    lab4

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +33,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late TextEditingController _nameController;
   late TextEditingController _passwordController;
+  final EncryptedSharedPreferences _prefs = EncryptedSharedPreferences();
+
   var myFontSize = 30.0;
   var imgQuestion = "images/question-mark.png";
   var imgIdea = "images/idea.png";
@@ -43,6 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
     _nameController = TextEditingController();
     _passwordController = TextEditingController();
     imgSource = imgQuestion;
+    _loadSavedCredentials();
   }
 
   @override
@@ -52,7 +57,53 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  void buttonClicked() {
+  void _loadSavedCredentials() async {
+    final savedUsername = await _prefs.getString('username');
+    final savedPassword = await _prefs.getString('password');
+
+    if (savedUsername != null && savedUsername.isNotEmpty && savedPassword != null && savedPassword.isNotEmpty) {
+      _nameController.text = savedUsername;
+      _passwordController.text = savedPassword;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Loaded saved username and password.')),
+      );
+    }
+  }
+
+  void _loginPressed() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Save Login Info"),
+          content: Text("Would you like to save your login info?"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("No"),
+              onPressed: () async {
+                await _prefs.remove('username');
+                await _prefs.remove('password');
+                Navigator.of(context).pop();
+                _handleLoginLogic();
+              },
+            ),
+            TextButton(
+              child: Text("Yes"),
+              onPressed: () async {
+                await _prefs.setString('username', _nameController.text);
+                await _prefs.setString('password', _passwordController.text);
+                Navigator.of(context).pop();
+                _handleLoginLogic();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _handleLoginLogic() {
     if (_passwordController.text == 'QWERTY123') {
       setState(() {
         imgSource = imgIdea;
@@ -67,19 +118,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var titleStyle = TextStyle(fontSize: myFontSize);
-    var numberStyle = Theme
-        .of(
-      context,
-    )
-        .textTheme
-        .headlineMedium
-        ?.copyWith(fontSize: myFontSize);
+    var numberStyle = Theme.of(context).textTheme.headlineMedium?.copyWith(fontSize: myFontSize);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
@@ -96,13 +138,14 @@ class _MyHomePageState extends State<MyHomePage> {
               obscureText: true,
             ),
             ElevatedButton(
-              onPressed: buttonClicked,
+              onPressed: _loginPressed,
               child: Text("Login"),
             ),
-            Image.asset(imgSource, height: 300, width: 300,),
+            Image.asset(imgSource, height: 300, width: 300),
           ],
         ),
       ),
     );
   }
 }
+
